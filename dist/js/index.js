abe4c899cb9067354215741a4c5bb8ed9fe2445c
wuguzi <run.what.time@foxmail.com>
2015-08-10 22:28:20 +0800

    GitCafe迁移到GitHub

---
+++ dist/js/index.js
@@ -0,0 +1,55 @@
+var map = new Map();
+
+//map.put('a', 1);
+//map.put('b', 2);
+//map.put('c', 3);
+//map.put('d', '4');
+//
+//console.log(map);
+
+var list = [100,200,300,400,20000,500,300,200,900];
+
+//var num = list.reduce(function(previous, current, index, array) {
+//  
+//  console.log(previous);
+//  console.log(current);
+//  console.log(index);
+//  console.log(array);
+//  return previous + current;
+//});
+
+//function decodeQuery(v) {
+//    var search = decodeURI(document.location.search);
+//    return search.replace(/(^\?)/, '').split('&').reduce(function(result, item){
+//      var values = item.split('=');
+//      result[values[0]] = values[1];
+//      return result;
+//    });
+//}
+//console.log(decodeQuery());
+//
+//function GetRequest() {
+//  
+//  var url = location.search; //获取url中"?"符后的字串
+//   var theRequest = new Object();
+//   if (url.indexOf("?") != -1) {
+//      var str = url.substr(1);
+//      strs = str.split("&");
+//      for(var i = 0; i < strs.length; i ++) {
+//         theRequest[strs[i].split("=")[0]]=(strs[i].split("=")[1]);
+//      }
+//   }
+//   return theRequest;
+//}
+//console.log(GetRequest());
+var s = new getSearch();
+console.log(s);
+
+
+
+
+
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'), 
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    rev = require('gulp-rev-append'),
+    htmlmin = require('gulp-htmlmin'),
+    watch = require('gulp-watch');
+
+var defaults = ['clean', 'html', 'javescript'];
+
+gulp.task('html', function() {
+  gulp.src('*.html')
+    .pipe(rev())
+    .pipe(gulp.dest('dist/html'))
+    .pipe(notify({ message: 'html finish' }));
+});
+
+gulp.task('javescript', function() {
+  gulp.src('js/lib/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('lib.js'))
+    .pipe(gulp.dest('dist/js/lib'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/lib'))
+    .pipe(notify({ message: 'javescript lib finish' }));  
+  
+  gulp.src('js/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(notify({ message: 'javescript finish' }));
+});
+
+gulp.task('clean', function() {
+  gulp.src(['dist/**/*.js'], {read: false})
+    .pipe(clean({force: true}))
+    .pipe(notify({ message: 'clean finish' }));
+});
+
+gulp.task('watch', function() { 
+  gulp.watch('js/**/*.js', defaults);
+});
+
+gulp.task('default', defaults);
